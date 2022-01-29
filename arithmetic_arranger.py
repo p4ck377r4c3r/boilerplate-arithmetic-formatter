946098afa1d437126f079ca66a77859377b17543
p4ck377r4c3r <98631738+p4ck377r4c3r@users.noreply.github.com>
2022-01-29 11:45:49 -0500

    Update arithmetic_arranger.py

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,46 +1,55 @@
-def arithmetic_arranger(problems, display_answers=False):
-    operands = []
-    row_1 = ''
-    row_2 = ''
-    equal_line = ''
-    equal_line_count = ''
-    answers = ''
-    
-    if len(problems)>5:
-        return 'Error: Too many problems.'
-    
-    for problem in problems:
-        if '*' or '/' in problem:
-            return 'Error: Operator must be ''+'' or ''-''.'
-        else:
-            operands.append(problem.split(' '))
-            
-    for i in range((len(operands))):
-        if not operands[i][0].isdigit() or not operands[i][2].isdigit():
-            return 'Error: Numbers must only contain digits.'
-        elif len(operands[i][0]) > 4 or len(operands[i][2] > 4:
-            return 'Error: Numbers cannot be more than four digits.'
-    
-    i = 0
-    for operand in operands:
-        if len(str(operand[0])) < len(str(operand[2])):
-            row_1 += ' ' + ' ' * (len(str(operand[2])) - len(str(operand[0]))) + str(operand[0])
-            row_2 += operand[1] + ' ' + str(operand[2])
-        else:
-            row_1 += ' ' str(operand[0])
-            row_2 += operand[1] + ' ' + ' ' * (len(str(operand[0])) - len(str(operand[2]))) + str(operand[2])
+def arithmetic_arranger(problems, *args):
+  if len(problems) > 5:
+    return "Error: Too many problems."
 
-        equal_line += '-' * (max(len(str(operand[0])), len(str(operand[2]))) + 2)
-        equal_line_count.append(max(len(str(operand[0])), len(str(operand[2]))) + 2)
-        if i == len(operand) -1:
-            row_1 += '\n'
-            row_2 += '\n'
-        else:
-            row_1 += '    '
-            row_2 += '    '
-            equal_line += '    '
-            i += 1
+  arranged_problems = []
+  
+  for index, value in enumerate(problems):
+    operation = value.split(" ")
+
+    if operation[1] not in "-+":
+      return "Error: Operator must be '+' or '-'."
+
+    if len(operation[0]) > 4 or len(operation[2]) > 4:
+      return "Error: Numbers cannot be more than four digits."
+
+    try:
+      value_1 = int(operation[0])
+      value_2 = int(operation[2])
+    except ValueError:
+      return "Error: Numbers must only contain digits."
+
+    longest_value = max(len(operation[0]), len(operation[2]))
+    width = longest_value + 2
+
+    L1 = f"{operation[0]:>{width}}"
+    L2 = operation[1] + f"{operation[2]:>{width-1}}"
+    L3 = '-' * width
+
+    try:
+      arranged_problems[0] += (' ' * 4) + L1
+    except IndexError:
+      arranged_problems.append(L1)
     
-    arranged_problems = row_1 + row_2 + equal_line
-    return arranged_problems
-                                            
+    try:
+      arranged_problems[1] += (' ' * 4) + L2
+    except IndexError:
+      arranged_problems.append(L2)
+
+    try:
+      arranged_problems[2] += (' ' * 4) + L3
+    except IndexError:
+      arranged_problems.append(L3)
+
+    answer = int(operation[0]) + int(operation[2]) if operation[1] == '+' else int(operation[0]) - int(operation[2])
+    L4 = f"{str(answer):>{width}}"
+    
+    if args:
+      try:
+        arranged_problems[3] += (' ' * 4) + L4
+      except IndexError:
+        arranged_problems.append(L4)
+
+  output = f"{arranged_problems[0]}\n{arranged_problems[1]}\n{arranged_problems[2]}"
+  output = output + f"\n{arranged_problems[3]}" if args else output
+  return output
